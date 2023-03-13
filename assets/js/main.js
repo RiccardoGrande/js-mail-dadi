96d037a392c2aebe5b44cca7f8f7a761dbc68a21
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-13 14:54:25 +0100

    add start first mail selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('funziona');
+//Chiedi all’utente la sua email
+    const mailEl = prompt("Qual'è la tua mail?")
+    console.log(mailEl);
+//controlla che sia nella lista di chi può accedere
+const mailList = [
+    'batman@gmail.com',
+    'daftpunkgmail.com',
+    'bethesda@gmail.com',
+    'cdprojectgmail.com'
+  ];
+  const mailSelect = mailList
+  console.log(mailList);
+  console.log(mailSelect);
+// stampa un messaggio appropriato sull’esito del controllo.
+
+if (mailEl == mailSelect) {
+    console.log('la mail è nella lista');
+} else {
+    console.log('la mail non è nella lista');
+}
