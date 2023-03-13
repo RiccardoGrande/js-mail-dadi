5e303044d3492f0042cfb45ee2545c0accce0504
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-13 15:07:37 +0100

    add fix message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 //Chiedi all’utente la sua email
     const mailEl = prompt("Qual'è la tua mail?")
     console.log(mailEl);
+    
 //controlla che sia nella lista di chi può accedere
 const mailList = [
     'batman@gmail.com',
@@ -8,12 +9,11 @@ const mailList = [
     'bethesda@gmail.com',
     'cdprojectgmail.com'
   ];
-  const mailSelect = mailList
-  console.log(mailList);
-  console.log(mailSelect);
+   console.log(mailList);
+  
 // stampa un messaggio appropriato sull’esito del controllo.
 
-if (mailEl == mailSelect) {
+if (mailList == mailEl) {
     console.log('la mail è nella lista');
 } else {
     console.log('la mail non è nella lista');
